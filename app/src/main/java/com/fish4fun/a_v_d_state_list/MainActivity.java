b38ad94281f3678cdcbc99d2dc7b01afa0127a3b
Jason Eggleton <13279993+fish-4-fun@users.noreply.github.com>
2017-09-17 17:31:15 -0600

    code cleanup + updated project settings

--- app/src/main/java/com/fish4fun/a_v_d_state_list/MainActivity.java
+++ app/src/main/java/com/fish4fun/a_v_d_state_list/MainActivity.java
@@ -1,9 +1,7 @@
 package com.fish4fun.a_v_d_state_list;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
