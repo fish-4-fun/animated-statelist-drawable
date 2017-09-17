c6a1dbb9a44d552c206b6fa89c2971846ab762c4
Jason Eggleton <bonefishky@gmail.com>
2017-09-17 17:18:26 -0600

    inital project commit

---
+++ app/src/main/java/com/fish4fun/a_v_d_state_list/MainActivity.java
@@ -0,0 +1,15 @@
+package com.fish4fun.a_v_d_state_list;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
